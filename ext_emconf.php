9e9dc9283197c89388ea2454615cf0d3e42c8c9e
Georg Großberger <contact@grossberger-ge.org>
2021-11-30 13:17:22 +0100

    Set correct version constraint in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,10 +22,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => '0',
     'createDirs'       => '',
     'clearCacheOnLoad' => 0,
-    'version'          => '0.3.0',
+    'version'          => '0.3.1',
     'constraints'      => [
         'depends' => [
-            'typo3' => '10.4.0-11.1.999',
+            'typo3' => '9.5.0-11.5.999',
         ],
     ],
 ];
